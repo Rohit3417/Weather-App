eb2d0d4373d22b006b7a6267b018a20c818749db
Rohit <mandalerohit54@gmail.com>
2026-06-06 20:17:50 +0530

    Final Version of Weather App

---
+++ WeatherApp/src/Main.java
@@ -0,0 +1,13 @@
+package WeatherApp.src;
+
+//import java.util.Scanner;
+
+//Mainly two api calls required :-
+//1). To get the latitude and longitude of a location (GeoLocation)
+//2). To get the weater info about the location ()
+
+public class Main {
+    public static void main(String[] args) {
+        UI.BuildUI();
+    }
+}
